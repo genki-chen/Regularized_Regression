5248092b1c734fb760e9c42c534cf92c27bc11d2
Huang Chen <chenhuang_cqupt@outlook.com>
2023-10-19 17:57:13 +0800

    [add] 1019

--- Regular_regression.py
+++ Regular_regression.py
@@ -1,25 +1,24 @@
 import numpy as np
-#from sklearn.linear_model import RidgeCV, LassoCV, ElasticNetCV, OrthogonalMatchingPursuitCV, LarsCV
+from sklearn.linear_model import RidgeCV, LassoCV, ElasticNetCV, OrthogonalMatchingPursuitCV, LarsCV
 from sklearn.linear_model import Ridge, Lasso, ElasticNet, LinearRegression
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from operator import itemgetter
+import seaborn as sns
+from scipy import stats
+from sklearn.metrics import mean_squared_error
 
 
 if __name__ == '__main__':
     ## 预设置
     show_ridge_b = False
-    show_lasso_b = False
-    show_ElasticNet_b = True
-    show_Top_num = 20
+    show_lasso_b = True
+    show_ElasticNet_b = False
+    show_Top_num = 10
     ## 读取数据
-    df = pd.read_csv('./x_data_precode.csv',index_col=0)
-    data_raw = df.values 
-    val_title = df.columns
-    df = pd.read_csv('./price.csv',index_col=0)
-    price = df.values.reshape(-1,)
+    df = pd.read_csv('./data_precode.csv',index_col=0)
     """
     df = pd.read_csv('./raw_data.csv',index_col=0)
     df.fillna('None',inplace=True)
@@ -34,21 +33,51 @@ if __name__ == '__main__':
             le.fit(data_raw[:,idx])
             data_raw[:,idx] = le.transform(data_raw[:,idx]) + 1
     """
+    """
+    ## 响应变量可视化
+    a=df
+    a['Sale_Price'] = np.log1p(df['Sale_Price'] )
+    sns.set(font_scale=2)
+    sns.displot(data=a,kde=True,height=10,aspect=1.6,x='Sale_Price')
+    plt.show()
+    plt.savefig('3.png')
+    plt.close()
+    stats.probplot(a['Sale_Price'],plot=plt)
+    plt.savefig('4.png')
+    fig = plt.figure(figsize=(8,8),dpi=300)
+    plt.scatter(x=df['Gr_Liv_Area'],y=df['Sale_Price'])
+    plt.xlabel('Gr_Liv_Area')
+    plt.ylabel('Sale_Price')
+    plt.title("Gr_Liv_Area vs Sale_Price", wrap=True)
+    plt.savefig('5.png')
+    plt.close(fig)
+    """
     ## 数据预处理
-    data_y = np.log1p(price)
+    df['Sale_Price'] = np.log1p(df['Sale_Price'])
+    df = df.drop(df[(df['Gr_Liv_Area']>4000)&(df['Sale_Price']<12.5)].index)
+    df = df.drop(df[(df['Gr_Liv_Area']<1000)&(df['Sale_Price']<10)].index)
+    data_raw = df.loc[:,df.columns != 'Sale_Price'].values 
+    val_title = df.loc[:,df.columns != 'Sale_Price'].columns
+    data_y = df['Sale_Price'].values
     #scaler = StandardScaler()  
     scaler = MinMaxScaler()
     scaler.fit(data_raw)
     x_scal = scaler.transform(data_raw)
     ## 数据分成训练集，测试集
-    x_train, x_test, y_train, y_test = train_test_split(x_scal,data_y,test_size=0.3,random_state=1)
+    x_train, x_test, y_train, y_test = train_test_split(x_scal,data_y,test_size=0.3,random_state=31)
     ## OLS
     clf_ols = LinearRegression().fit(x_train,y_train)
+    y_test_pre = clf_ols.predict(x_test)
+    res_log_ols = ((y_test_pre-y_test)/y_test)**2
+    var_res_ols = np.var(res_log_ols)
+    mse_ols = "OLS MSE = "+"{:.3e}".format(mean_squared_error(y_test,y_test_pre))+" and Var(res.) = "+"{:.2e}".format(var_res_ols)
+    #print("OLS MSE = "+str(np.round(mean_squared_error(y_test,y_test_pre),4)))
     print("OLS score = "+str(np.max([np.round(clf_ols.score(x_test,y_test),4),0])))
     print("===========================================")
     ## 岭回归
-    #clf_ridge = RidgeCV(alphas=np.logspace(-3,2,500)).fit(x_train,y_train)
-    clf_ridge = Ridge(alpha=7).fit(x_train,y_train)
+    clf_ridge = RidgeCV(alphas=np.logspace(-3,2,500),store_cv_values=True).fit(x_train,y_train)
+    y_test_pre = clf_ridge.predict(x_test)
+    #clf_ridge = Ridge(alpha=7).fit(x_train,y_train)
     if show_ridge_b:
         Ridge_coef = clf_ridge.coef_
         coef_dict = {}
@@ -63,32 +92,61 @@ if __name__ == '__main__':
             count += 1
             if count > show_Top_num:
                 break
-    #print("Ridge alpha = " + str(np.round(clf_ridge.alpha_,4)))
+    res_log_ridge = ((y_test_pre-y_test)/y_test)**2
+    var_res_ridge = np.var(res_log_ridge)
+    mse_ridge = "Ridge MSE = "+str(np.round(mean_squared_error(y_test,y_test_pre),4))+" and Var(res.) = "+"{:.2e}".format(var_res_ridge)
+    """
+    fig = plt.figure(figsize=(14,8),dpi=300)
+    plt.plot(np.logspace(-3,2,500),np.mean(clf_ridge.cv_values_,axis=0),lw=2,ls='-',color=(0.9,0.05,0.05))
+    plt.plot(clf_ridge.alpha_,np.mean(clf_ridge.cv_values_[:,np.where(np.logspace(-3,2,500)==clf_ridge.alpha_)]),marker='*',mfc=(0.75,0.1,0.1,0.8),mec='k',ms=12)
+    plt.xscale('log')
+    plt.xlabel('log(lambda)')
+    plt.ylabel('MSE')
+    plt.grid(True,which='both',linestyle='--',axis='y')
+    plt.savefig('6.png')
+    plt.close(fig)
+    """
+    print("Ridge alpha = " + str(np.round(clf_ridge.alpha_,4)))
     print("Ridge score = "+ str(np.round(clf_ridge.score(x_test,y_test),4)))
     print("===========================================")
     ## Lasso回归
-    #clf_lasso = LassoCV(cv=5,random_state=0,alphas=np.logspace(-4,2,100),max_iter=10000,n_jobs=-1).fit(x_train,y_train)
-    clf_lasso = Lasso(alpha=0.0005).fit(x_train,y_train)
+    clf_lasso = LassoCV(cv=5,random_state=0,alphas=np.logspace(-4,2,100),max_iter=10000,n_jobs=-1).fit(x_train,y_train)
+    #clf_lasso = Lasso(alpha=0.0005).fit(x_train,y_train)
     if show_lasso_b:
         Lasso_coef = clf_lasso.coef_
-        coef_dict = {}
+        coef_list = []
         idx = 0
         for var in Lasso_coef:
-            coef_dict[val_title[idx]] = np.abs(var)
+            #coef_dict[val_title[idx]] = np.abs(var)
+            coef_list = np.append(coef_list,{'coff_name':val_title[idx],'coff_val':var})
             idx += 1
-        sorted_dict = dict(sorted(coef_dict.items(), key=itemgetter(1),reverse=True))
+        sorted_dict = sorted(coef_list, key=lambda x: np.abs(x['coff_val']),reverse=True)
         count = 1  # 用于计数前10个键值对
-        for key, value in sorted_dict.items():
-            print(str(count)+' '+key+' '+str(np.round(value,4)))
+        for dicts in sorted_dict:
+            print(str(count)+' ,'+dicts['coff_name']+' ,'+str(np.round(dicts['coff_val'],4)))
             count += 1
             if count > show_Top_num:
                 break
-    #print("Lasso alpha = " + str(np.round(clf_lasso.alpha_,4)))
+
+        """
+        sorted_dict = dict(sorted(coef_dict.items(), key=itemgetter(1),reverse=True))
+        count = 1  # 用于计数前10个键值对
+        for key, value in sorted_dict.items():
+            print(str(count)+' ,'+key+' ,'+str(np.round(value,4)))
+            count += 1
+            if count > show_Top_num:
+                break
+        """
+    print("Lasso alpha = " + str(np.round(clf_lasso.alpha_,4)))
+    y_test_pre = clf_lasso.predict(x_test)
+    res_log_lasso = ((y_test_pre-y_test)/y_test)**2
+    var_res_lasso = np.var(res_log_lasso)
+    mse_lasso = "Lasso MSE = "+str(np.round(mean_squared_error(y_test,y_test_pre),4))+" and Var(res.) = "+"{:.2e}".format(var_res_lasso)
     print("Lasso score = " + str(np.round(clf_lasso.score(x_test,y_test),4)))
     print("===========================================")
     ## 弹性网
-    #clf_elasticNet = ElasticNetCV(l1_ratio=np.linspace(0.1,1,11),alphas=np.logspace(-4,2,100),random_state=0,max_iter=10000,n_jobs=-1).fit(x_train,y_train)
-    clf_elasticNet = ElasticNet(l1_ratio=0.05,alpha=0.0019).fit(x_train,y_train)
+    clf_elasticNet = ElasticNetCV(l1_ratio=np.linspace(0.1,1,11),alphas=np.logspace(-4,2,100),random_state=0,max_iter=10000,n_jobs=-1).fit(x_train,y_train)
+    #clf_elasticNet = ElasticNet(l1_ratio=0.05,alpha=0.0019).fit(x_train,y_train)
     if show_ElasticNet_b:
         ElasticNet_coef = clf_elasticNet.coef_
         coef_dict = {}
@@ -103,9 +161,25 @@ if __name__ == '__main__':
             count += 1
             if count > show_Top_num:
                 break
-    #print("ElasticNet alpha = " + str(np.round(clf_elasticNet.alpha_,4)))
-    #print("ElasticNet l1_ratio = " + str(np.round(clf_elasticNet.l1_ratio_,2)))
+    print("ElasticNet alpha = " + str(np.round(clf_elasticNet.alpha_,4)))
+    print("ElasticNet l1_ratio = " + str(np.round(clf_elasticNet.l1_ratio_,2)))
+    y_test_pre = clf_elasticNet.predict(x_test)
+    res_log_elasticNet = ((y_test_pre-y_test)/y_test)**2
+    var_res_net = np.var(res_log_elasticNet)
+    mse_elasticNet = "ElasticNet MSE = "+str(np.round(mean_squared_error(y_test,y_test_pre),4))+" and Var(res.) = "+"{:.2e}".format(var_res_net)
     print("ElasticNet score = " + str(np.round(clf_elasticNet.score(x_test,y_test),4)))
+
+    fig = plt.figure(figsize=(14,8),dpi=300)
+    plt.scatter(np.arange(len(y_test)),res_log_ols,c='orange',label=mse_ols,alpha=0.5,edgecolors='none')
+    plt.scatter(np.arange(len(y_test)),res_log_ridge,c='r',label=mse_ridge,alpha=0.5,edgecolors='none')
+    plt.scatter(np.arange(len(y_test)),res_log_lasso,c='g',label=mse_lasso,alpha=0.5,edgecolors='none')
+    plt.scatter(np.arange(len(y_test)),res_log_elasticNet,c='b',label=mse_elasticNet,alpha=0.5,edgecolors='none')
+    plt.legend(loc=1,fontsize='large',framealpha=0.7)
+    plt.xlabel('Test idx.')
+    plt.ylabel('log(residual)')
+    plt.yscale('log')
+    plt.grid(True,which='both',linestyle='--',axis='both')
+    plt.savefig('7.png')
 
     #clf_lars = LarsCV(eps=1e-2).fit(x_train,y_train.ravel())
     #print(clf_lars.score(x_test,y_test.ravel()))
---
+++ demo.py
@@ -0,0 +1,55 @@
+import numpy as np
+from sklearn.linear_model import Ridge, Lasso, ElasticNet
+from sklearn.preprocessing import  MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+
+if __name__ == '__main__':
+    ## 预设置
+    test_rate_list = np.linspace(0.95,0.05,20)
+    repeat_num = 1000
+    ## 读取数据
+    df = pd.read_csv('./data_precode.csv',index_col=0)
+    ## 数据预处理
+    df['Sale_Price'] = np.log1p(df['Sale_Price'])
+    df = df.drop(df[(df['Gr_Liv_Area']>4000)&(df['Sale_Price']<12.5)].index)
+    df = df.drop(df[(df['Gr_Liv_Area']<1000)&(df['Sale_Price']<10)].index)
+    data_raw = df.loc[:,df.columns != 'Sale_Price'].values 
+    val_title = df.loc[:,df.columns != 'Sale_Price'].columns
+    data_y = df['Sale_Price'].values
+    scaler = MinMaxScaler()
+    scaler.fit(data_raw)
+    x_scal = scaler.transform(data_raw)
+
+    MSE_arr = np.zeros((len(test_rate_list),repeat_num,3))
+    for idx_i in range(len(test_rate_list)):
+        for idx_j in range(repeat_num):
+            ## 数据分成训练集，测试集
+            x_train, x_test, y_train, y_test = train_test_split(x_scal,data_y,test_size=test_rate_list[idx_i])
+            ## 岭回归
+            clf_ridge = Ridge(alpha=2.381).fit(x_train,y_train)
+            y_test_pre = clf_ridge.predict(x_test)
+            MSE_arr[idx_i,idx_j,0] = mean_squared_error(y_test,y_test_pre)
+            ## Lasso回归
+            clf_lasso = Lasso(alpha=0.0002,max_iter=2000).fit(x_train,y_train)
+            y_test_pre = clf_lasso.predict(x_test)
+            MSE_arr[idx_i,idx_j,1] = mean_squared_error(y_test,y_test_pre)
+            ## 弹性网
+            clf_elasticNet = ElasticNet(l1_ratio=0.55,alpha=0.0003).fit(x_train,y_train)
+            y_test_pre = clf_elasticNet.predict(x_test)
+            MSE_arr[idx_i,idx_j,2] = mean_squared_error(y_test,y_test_pre)
+    np.save('./data.npy',MSE_arr)
+    MSE_mean = np.mean(MSE_arr,axis=1)
+    print(MSE_mean.shape)
+    fig = plt.figure(figsize=(16,10),dpi=300)
+    plt.plot((1-test_rate_list)*100,MSE_mean[:,0],lw=2,ls='-',color='blue',label='Ridge')
+    plt.plot((1-test_rate_list)*100,MSE_mean[:,1],lw=2,ls='-.',color='red',label='Lasso')
+    plt.plot((1-test_rate_list)*100,MSE_mean[:,2],lw=2,ls='--',color='g',label='ElasticNet')
+    plt.xlabel('Training set size(%)')
+    plt.ylabel('MSE')
+    plt.legend(loc=1,fontsize='large',framealpha=0.7)
+    plt.grid(True,which='both',linestyle='--')
+    plt.savefig('./10.png')
